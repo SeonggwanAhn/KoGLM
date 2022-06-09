8eedc679ecd0e2680254c829668bbc55ca793630
SeonggwanAhn <policean95@gmail.com>
2022-06-10 02:47:13 +0900

    Modify finetune_seq2seq shell for distributed train

---
+++ scripts/finetune_seq2seq_copy.sh
@@ -0,0 +1,29 @@
+DATA_ROOT=/data/sgahn/seq2seq
+CHECKPOINT_PATH=/data/sgahn/ckpt
+SAVE_PATH=/data/sgahn/ckpt
+DATESTR=$(date +"%m-%d-%H-%M")
+
+source $1    # Model
+source $2    # Task
+
+N_GPU=1
+MASTER_PORT=$(shuf -n 1 -i 10000-65535)
+DISTRIBUTED_ARGS="--nproc_per_node ${N_GPU} --nnodes 1 --node_rank 0 --master_addr localhost --master_port $MASTER_PORT"
+
+
+EXPERIMENT_NAME=${EXPERIMENT_NAME}_${DATESTR}
+run_cmd="python -m torch.distributed.launch ${DISTRIBUTED_ARGS} finetune_glm.py \
+       --finetune \
+       --experiment-name ${EXPERIMENT_NAME} \
+       --task ${TASK_NAME} \
+       --data-dir ${DATA_PATH} \
+       --save ${CHECKPOINT_PATH} \
+       --checkpoint-activations \
+       $MODEL_ARGS \
+       $TRAIN_ARGS \
+       $COMMON_ARGS \
+       $TASK_ARGS \
+       2>&1 | tee /data/sgahn/logs/log-${EXPERIMENT_NAME}.txt"
+
+echo ${run_cmd}
+eval ${run_cmd}
