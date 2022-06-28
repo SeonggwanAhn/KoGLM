99d8e0a8477b647ed5a30d6955f4f19b7d55d6ef
SeonggwanAhn <policean95@gmail.com>
2022-06-28 22:29:00 +0900

    Change hparams

--- config/kor_block_base.sh
+++ config/kor_block_base.sh
@@ -14,20 +14,24 @@ gpt_options=" \
        --num-attention-heads 12 \
        --seq-length 512 \
        --max-position-embeddings 512 \
-       --save /data/sgahn/ckpt \
-       --train-iters 150000 \
+       --save /home/tempuser/ckpt \
+       --train-iters 3000000 \
        --resume-dataloader \
-       --train-data online_review nikl_newspaper nikl_kparlty nikl_daily nikl_newspaper_2020 nikl_messenger nikl_newspaper_2021 nikl_spoken nikl_written nikl_om \
+       --train-data nikl_newspaper online_review nikl_kparlty nikl_daily nikl_newspaper_2020 nikl_messenger nikl_newspaper_2021 nikl_spoken nikl_written nikl_om \
        --tokenizer-type BertWordPieceTokenizer \
        --tokenizer-model-type monologg/koelectra-base-v3-discriminator \
        --split 949,50,1 \
        --distributed-backend nccl \
        --lr-decay-style cosine \
-       --lr-decay-iters 120000 \
        --lr-decay-ratio 0.05 \
        --warmup .05 \
        --checkpoint-activations \
-       --batch-size 64 \
-       --fp16
-
+       --batch-size 128 \
+       --adam-beta2 0.98 \
+       --adam-eps 1e-6 \
+       --weight-decay 0.1 \
+	   --lr 0.0004 \
+       --fp16 \
+       --eval-interval 10000 \
+	   --save-interval 100000 \
 "
